a3a78d3a9429acf3229cd6fd5a48bc78cbd01d0a
Dmytro <dimarubalchenk@ukr.net>
2020-10-12 19:03:51 +0300

    Finish base framework functional

---
+++ src/components/table/Table.js
@@ -0,0 +1,23 @@
+import { ExcelComponent } from "../../core/ExcelComponent";
+import { createTable } from "./table.template";
+
+export class Table extends ExcelComponent {
+    static className= 'excel__table'
+
+    constructor($root) {
+      super($root, {
+        name: 'Table',
+        listeners: ['click']
+      })
+    }
+  
+
+    toHTML() {
+        return createTable(50)
+    
+    }
+
+    onClick(event) {
+      console.log(event.target);
+    }
+}
